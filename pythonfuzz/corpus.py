6f9db681615de600d5d9d90b6196f7bbb8cd5c03
Alexander Heinrich <hallo@alexanderheinrich.de>
2021-06-29 10:38:29 +0200

    Use randint instead of randbelow for compatibility

--- pythonfuzz/corpus.py
+++ pythonfuzz/corpus.py
@@ -46,7 +46,7 @@ class Corpus(object):
     def _rand(n):
         if n < 2:
             return 0
-        return random._randbelow(n)
+        return random.randint(0, n-1)
 
     # Exp2 generates n with probability 1/2^(n+1).
     @staticmethod
